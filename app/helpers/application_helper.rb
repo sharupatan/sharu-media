b4a3eea0df4689cfe3236e6738844063b4e17399
sharupatan <patansharu654321@gmail.com>
2023-08-22 02:47:59 +0530

    added navbar, added authentication btn

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def signin_or_signout_btn
+		if user_signed_in?
+			button_to :sign_out, destroy_user_session_path, method: :delete, class:'nav-link'
+		else
+			link_to :sign_in, new_user_session_path, class:'nav-link'
+		end
+	end
 end
